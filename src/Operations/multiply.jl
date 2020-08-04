3801b01940213fecad32176bfe53e7316211a23e
Shisato <shisatoyano@gmail.com>
2020-08-04 23:32:35 +0900

    add omit * pattern

--- src/Operations/multiply.jl
+++ src/Operations/multiply.jl
@@ -12,5 +12,12 @@ end
 if abspath(PROGRAM_FILE) == @__FILE__
     a = 15
     b = 12
-    println("$a × $b = $(multiply(a, b))")
+    println("a = $a, b = $b")
+    println()
+    println("use *")
+    println("a * b = $(multiply(a, b))")
+    println()
+    println("omit *")
+    println("2a + b = $(2a + b)")
+    println("(a + 2)b = ", (a + 2)b)
 end
