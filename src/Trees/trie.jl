c1344c76006fb65d097d5238407822d1f4c28a56
Shisato <shisatoyano@gmail.com>
2020-08-29 00:24:24 +0900

    implemented insert and search

--- src/Trees/trie.jl
+++ src/Trees/trie.jl
@@ -11,12 +11,47 @@ mutable struct TrieNode
     is_end
 end
 
+const alphabet_num = 26
+
 function get_node()
-    return TrieNode([nothing for i in 1:26], false)
+    return TrieNode([TrieNode([], false) for i in 1:alphabet_num], false)
+end
+
+function char_to_idx(ch)
+    if Int(ch) - Int('a') == 0
+        return 1
+    else
+        return Int(ch) - Int('a')
+    end
 end
 
 function insert(key)
+    # if not present, inserts key into trie
+    # if the key is prefix of trie node,
+    # just marks leaf node
+    p_crawl = root
+    len_key = length(key)
+    for level in 1:len_key
+        idx = char_to_idx(key[level])
+        if isempty(p_crawl.children[idx].children) == true
+            p_crawl.children[idx] = get_node()
+        end
+        p_crawl = p_crawl.children[idx]
+    end
+    p_crawl.is_end = true
+end
 
+function search(key)
+    p_crawl = root
+    len_key = length(key)
+    for level in 1:len_key
+        idx = char_to_idx(key[level])
+        if isempty(p_crawl.children[idx].children) == true
+            return false
+        end
+        p_crawl = p_crawl.children[idx]
+    end
+    return isempty(p_crawl.children) == false && p_crawl.is_end == true
 end
 
 function main()
@@ -28,12 +63,15 @@ function main()
     output = ["Not present", "Present"]
 
     # init root
-    root = get_node()
+    global root = get_node()
 
     # construct trie
     for key in keys
-        println(key)
+        insert(key)
     end
+
+    # search different key
+    println(search("thaw"))
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
