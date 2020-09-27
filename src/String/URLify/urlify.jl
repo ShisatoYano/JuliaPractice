f72fb97aae2565a08b16183907ed4573bcb4083f
Shisato <shisatoyano@gmail.com>
2020-09-27 23:24:28 +0900

    started implementing

--- src/String/URLify/urlify.jl
+++ src/String/URLify/urlify.jl
@@ -4,3 +4,22 @@ urlify:
 - Author: shisa
 - Date: 2020-09-27
 =#
+
+module URLify
+    # parameters
+    const max_len = 1000
+
+    # methods
+    function replace_space(str)
+
+    end
+
+    function main()
+        println(replace_space("Mr John Smith "))
+    end
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    using .URLify
+    URLify.main()
+end
