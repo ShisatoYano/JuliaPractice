700cfe3dc45570e6f507d7e9cc44a115e1773559
Shisato <shisatoyano@gmail.com>
2020-09-28 23:26:36 +0900

    fill string array

--- src/String/URLify/urlify.jl
+++ src/String/URLify/urlify.jl
@@ -11,11 +11,38 @@ module URLify
 
     # methods
     function replace_space(str)
+        # remove leading and trailing spaces
+        str = strip(str, [' '])
 
+        # check current lenght and count spaces
+        len_str = length(str)
+        spc_count = count(i->(i==' '), str)
+
+        # calculate new length
+        new_len = len_str + spc_count * 2
+        if new_len > max_len
+            println("New lenght is longer than maximum")
+            return -1
+        end
+
+        # start index filling char from end
+        index = new_len
+
+        str_sub = split(str, "")
+
+        # fill string array
+        for i in (len_str-2):(new_len-3)
+            push!(str_sub, "0")
+        end
+
+        # fill rest of string from end
+        for i in (len_str):-1:1
+            println(str_sub[i])
+        end
     end
 
     function main()
-        println(replace_space("Mr John Smith "))
+        println(replace_space(" Mr John Smith "))
     end
 end
 
