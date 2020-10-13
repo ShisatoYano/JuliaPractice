55219d42216695bdc693d75dbbfa2cb9783e4e35
Shisato <shisatoyano@gmail.com>
2020-10-13 22:19:57 +0900

    rename

--- src/Search/HillClimbingMethod/hill_climbing_method.jl
+++
@@ -1,6 +0,0 @@
-#=
-hill_climbing_method:
-- Julia version: 1.5.0
-- Author: shisa
-- Date: 2020-10-13
-=#
---
+++ src/Search/HillClimbingMethod/hill_climbing_search.jl
@@ -0,0 +1,12 @@
+#=
+hill_climbing_search:
+- Julia version: 1.5.0
+- Author: shisa
+- Date: 2020-10-13
+
+Goal is as top of mountain.
+Searching to climbing direction.
+Cost(distance to Goal) need to be calculated.
+Evaluated by Manhattan distance.
+=#
+
