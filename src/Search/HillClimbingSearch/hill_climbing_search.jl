7caf9b3b3d1f10595b0baa48ff652186060fa2d2
Shisato <shisatoyano@gmail.com>
2020-10-15 23:30:26 +0900

    started implementing search function

--- src/Search/HillClimbingSearch/hill_climbing_search.jl
+++ src/Search/HillClimbingSearch/hill_climbing_search.jl
@@ -32,15 +32,17 @@ module HCS
     # distance to other nodes
     const distance = (
         (),
-        (0, 1, 2, 1, 2, 3, 2, 3, 4),
-        (1, 0, 1, 2, 1, 2, 3, 2, 3),
-        (2, 1, 0, 3, 2, 1, 4, 3, 2),
-        (1, 2, 3, 0, 1, 2, 1, 2, 3),
-        (2, 1, 2, 1, 0, 1, 2, 1, 2),
-        (3, 2, 1, 2, 1, 0, 3, 2, 1),
-        (2, 3, 4, 1, 2, 3, 0, 1, 2),
-        (3, 2, 3, 2, 1, 2, 1, 0, 1)
+        (0, 1, 2, 1, 2, 3, 2, 3, 4), # 1
+        (1, 0, 1, 2, 1, 2, 3, 2, 3), # 2
+        (2, 1, 0, 3, 2, 1, 4, 3, 2), # 3
+        (1, 2, 3, 0, 1, 2, 1, 2, 3), # 4
+        (2, 1, 2, 1, 0, 1, 2, 1, 2), # 5
+        (3, 2, 1, 2, 1, 0, 3, 2, 1), # 6
+        (2, 3, 4, 1, 2, 3, 0, 1, 2), # 7
+        (3, 2, 3, 2, 1, 2, 1, 0, 1)  # 8
     )
+
+    const goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
     # distance to goal
     function get_distance(board)
@@ -54,10 +56,28 @@ module HCS
         return dist
     end
 
-    function main()
-        board = [8, 6, 7, 2, 5, 4, 3, 0, 1]
+    # hill clibming search
+    function search(board, space, cost, history)
+        if board == goal
+            return true
+        else
+            buffer = []
+            for i in adjacent[space]
+                panel = board[i+1]
+                b_tmp = board # need to copy
+                b_tmp[space] = b_tmp[i+1]
+                b_tmp[i+1] = 0
+                println(panel,"",i)
+            end
+        end
+        return false
+    end
 
-        println(get_distance(board))
+    function main()
+        # initial board
+        b = [8, 6, 7, 2, 5, 4, 3, 0, 1]
+
+        search(b, findall(x->x == 0, b)[1], get_distance(b), [b])
     end
 end
 
