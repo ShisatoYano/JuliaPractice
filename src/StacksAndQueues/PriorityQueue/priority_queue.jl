f17f76ff39dd57f96224768427711088eeed5806
Shisato <shisatoyano@gmail.com>
2020-10-19 22:55:11 +0900

    implemented downheap

--- src/StacksAndQueues/PriorityQueue/priority_queue.jl
+++ src/StacksAndQueues/PriorityQueue/priority_queue.jl
@@ -24,6 +24,30 @@ module PQueue
         end
     end
 
+    # move root node to leaf
+    function downheap(buffer, n)
+        len = length(buffer)
+        while true
+            c = 2 * n + 1
+            if c >= len
+                break
+            end
+            # exchange parent and smaller child
+            if (c + 1) < len
+                if buffer[c+1] > buffer[c+2]
+                    c += 1
+                end
+            end
+            if buffer[n+1] <= buffer[c+1]
+                break
+            end
+            tmp = buffer[n+1]
+            buffer[n+1] = buffer[c+1]
+            buffer[c+1] = tmp
+            n = c
+        end
+    end
+
     # add data to tail of buffer
     # after adding, sort as upheap
     function push(buffer, data)
@@ -31,11 +55,34 @@ module PQueue
         upheap(buffer, length(buffer)-1)
     end
 
+    # get minimum data
+    function peek(buffer)
+        return buffer[1]
+    end
+    
+    # get and remove minimum data
+    function pop(buffer)
+        min_val = buffer[1]
+        last_val = buffer[end]
+        deleteat!(buffer, length(buffer))
+        if length(buffer) > 0
+            # create heap again
+            buffer[1] = last_val
+            downheap(buffer, 0)
+        end
+        return min_val
+    end
+
     function main()
         input = sample(1:100, 10, replace=false)
-        # create upheap
+        # create by upheap
         for n in input
             push(buffer, n)
+            println("Add $(n), Min data = $(peek(buffer))")
+        end
+        # remove minimum data and create by downheap
+        while length(buffer) > 0
+            print("$(pop(buffer)) ")
         end
     end
 end
