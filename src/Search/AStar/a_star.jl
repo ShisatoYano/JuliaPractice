4d95586d90b300474492b5e3b066217ee1539892
Shisato <shisatoyano@gmail.com>
2020-11-03 21:51:12 +0900

    implemented main function

--- src/Search/AStar/a_star.jl
+++ src/Search/AStar/a_star.jl
@@ -4,3 +4,22 @@ a_star:
 - Author: shisa
 - Date: 2020-11-02
 =#
+module AStar
+    function search(start)
+
+    end
+
+    function main()
+        # inital board
+        start = [8, 6, 7, 2, 5, 4, 3, 0, 1]
+
+        # searching by A* algorithm
+        @time search(start)
+    end
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    using .AStar
+    as = AStar
+    as.main()
+end
